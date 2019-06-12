4b52160c03c0bce8bc0ca6ef5e918d990f812e17
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2019-06-12 14:22:21 +0600

    Created controller for register-computer route and wrote the logic of saving a record and giving back the record's id

---
+++ app/Http/Controllers/ComputerController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Computer;
+use Illuminate\Http\Request;
+
+class ComputerController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $computer = Computer::create(['ip' => $request->ip()]);
+        return response()->json(['status' => 200, 'id' => $computer->id]);
+    }
+}
