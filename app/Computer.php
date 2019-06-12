08ad0e4bfc983a583659372ee3274e62439231dd
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2019-06-12 14:23:37 +0600

    Added table name and added row id in fillabe property

--- app/Computer.php
+++ app/Computer.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Computer extends Model
 {
-    //
+    /*
+     * Table name for current Model
+     */
+    protected $table = 'computers';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['ip'];
+
 }
