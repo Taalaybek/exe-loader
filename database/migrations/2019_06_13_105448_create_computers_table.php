96dbe9895b8bcd247d78e357a9b6bdd2ede3d251
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2019-06-13 16:57:42 +0600

    Created migrations for tasks and computers

---
+++ database/migrations/2019_06_13_105448_create_computers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComputersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('computers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char('ip', 255);
+            $table->string('status')->default('offline');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('computers');
+    }
+}
