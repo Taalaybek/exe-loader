d16b2330ae40cf41f167784831026da5a8444a29
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2019-06-15 22:20:31 +0600

    Created command to add super admin with username

---
+++ app/Console/Commands/AdminCreating.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\Console\Input\InputOption;
+use TCG\Voyager\Facades\Voyager;
+
+class AdminCreating extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $name = 'make:admin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Make sure there is a user with the admin role that has all of the necessary permissions.';
+
+    /**
+     * Get user options.
+     */
+    protected function getOptions()
+    {
+        return [
+            ['create', null, InputOption::VALUE_NONE, 'Create an admin user', null],
+        ];
+    }
+
+    public function fire()
+    {
+        return $this->handle();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // Get or create user
+        $user = $this->getUser(
+            $this->option('create')
+        );
+
+        // the user not returned
+        if (!$user) {
+            exit;
+        }
+
+        // Get or create role
+        $role = $this->getAdministratorRole();
+
+        // Get all permissions
+        $permissions = Voyager::model('Permission')->all();
+
+        // Assign all permissions to the admin role
+        $role->permissions()->sync(
+            $permissions->pluck('id')->all()
+        );
+
+        // Ensure that the user is admin
+        $user->role_id = $role->id;
+        $user->save();
+
+        $this->info('The user now has full access to your site.');
+    }
+
+    /**
+     * Get command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['email', InputOption::VALUE_REQUIRED, 'The email of the user.', null],
+        ];
+    }
+
+    /**
+     * Get the administrator role, create it if it does not exists.
+     *
+     * @return mixed
+     */
+    protected function getAdministratorRole()
+    {
+        $role = Voyager::model('Role')->firstOrNew([
+            'name' => 'admin',
+        ]);
+
+        if (!$role->exists) {
+            $role->fill([
+                'display_name' => 'Administrator',
+            ])->save();
+        }
+
+        return $role;
+    }
+
+    /**
+     * Get or create user.
+     *
+     * @param bool $create
+     *
+     * @return \App\User
+     */
+    protected function getUser($create = false)
+    {
+        $email = $this->argument('email');
+
+        $model = config('voyager.user.namespace') ?: config('auth.providers.users.model');
+
+        // If we need to create a new user go ahead and create it
+        if ($create) {
+            $username = $this->ask('Enter the username');
+            $name = $this->ask('Enter the admin name');
+            $password = $this->secret('Enter admin password');
+            $confirmPassword = $this->secret('Confirm Password');
+
+            // Ask for username if there wasn't set one
+            if (!$username) {
+                $username = $this->ask('Enter the username');
+            }
+
+            // Ask for email if there wasn't set one
+            if (!$email) {
+                $email = $this->ask('Enter the admin email');
+            }
+
+            // Passwords don't match
+            if ($password != $confirmPassword) {
+                $this->info("Passwords don't match");
+
+                return;
+            }
+
+            $this->info('Creating admin account');
+            $this->info($username . ' created');
+            return $model::create([
+                'name'     => $name,
+                'username' => $username,
+                'email'    => $email,
+                'password' => Hash::make($password),
+            ]);
+        }
+
+        return $model::where('where', $email)->firstOrFail();
+    }
+}
