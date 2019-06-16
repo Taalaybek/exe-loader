2d3032073e8a3ed3055e7b431a5408a6108d9179
Taalay Eshen <taalaybekeshenov.96@gmail.com>
2019-06-16 22:59:08 +0600

    Changed to OverrodeRequest

--- app/Http/Controllers/ComputerController.php
+++ app/Http/Controllers/ComputerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Computer;
+use App\Services\OverrodeRequest;
 use Illuminate\Http\Request;
 
 class ComputerController extends Controller
@@ -15,7 +16,7 @@ class ComputerController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $computer = Computer::create(['ip' => $request->ip()]);
+        $computer = Computer::create(['ip' => (new OverrodeRequest())->ip()]);
         return response()->json(['status' => 200, 'id' => $computer->id]);
     }
 }
